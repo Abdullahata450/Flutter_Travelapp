559013a0d926960f5244647186aef0c72257c6b1
Abdullahata450 <abdullahbinata450@gmail.com>
2024-04-29 02:02:36 +0500

    Updated and Added New Features

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import '../Cities/IslamabadPostScreen.dart';
+import '../Cities/KPKpostScreen.dart';
+import '../Cities/KarchipostScreen.dart';
+import '../Cities/KashmirPostScreen.dart';
+import '../Cities/LahorepostScreen.dart';
+import '../Cities/MultanPostScreen.dart';
 import '../OtherModule/FlightBooking.dart';
 import '../theams/BottomNavbar.dart';
 import '../theams/Homeappbar.dart';
@@ -26,6 +32,16 @@ class Homepage extends StatelessWidget {
     "Map",
   ];
 
+  // List of routes for each city post screen
+  List<dynamic> cityRoutes = [
+    LahorePostScreen(),
+    KarachipostScreen(),
+    IslamabadPostScreen(),
+    MultanPostScreen(),
+    KPKpostScreen(),
+    KashmirPostScreen()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -50,7 +66,15 @@ class Homepage extends StatelessWidget {
                       shrinkWrap: true,
                       itemBuilder: (BuildContext context, int index) {
                         return InkWell(
-                          onTap: () {},
+                          onTap: () {
+                            // Navigate to the corresponding city post screen
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => cityRoutes[index],
+                              ),
+                            );
+                          },
                           child: Container(
                             width: 150,
                             padding: EdgeInsets.all(20),
@@ -92,7 +116,9 @@ class Homepage extends StatelessWidget {
                       },
                     ),
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(
+                    height: 20,
+                  ),
                   SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
                     child: Padding(
@@ -102,21 +128,23 @@ class Homepage extends StatelessWidget {
                           for (int i = 0; i < 5; i++)
                             GestureDetector(
                               onTap: () {
-                               switch(i){
-                                 case 0:
-                                   Navigator.push(
-                                     context,
-                                     MaterialPageRoute(builder: (context) => HomeScreen()),
-                                   );
-                                   break;
-                                 case 1:
-                                   Navigator.push(
-                                     context,
-                                     MaterialPageRoute(builder: (context) => FlightBookingPage()),
-                                   );
-                                   break;
-                               }
-
+                                switch (i) {
+                                  case 0:
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => HomeScreen()),
+                                    );
+                                    break;
+                                  case 1:
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              FlightBookingPage()),
+                                    );
+                                    break;
+                                }
                               },
                               child: Container(
                                 margin: EdgeInsets.symmetric(horizontal: 10),
@@ -142,66 +170,96 @@ class Homepage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(
+                    height: 20,
+                  ),
                   Text(
                     "Trending Destination in Pakistan",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700
-                    ),
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(
+                    height: 20,
+                  ),
                   ListView.builder(
-                    physics: NeverScrollableScrollPhysics(), // Disable scrolling
+                    physics: NeverScrollableScrollPhysics(),
+                    // Disable scrolling
                     shrinkWrap: true,
-                    itemCount: 3, // Set the number of items
+                    itemCount: 6, // Changed itemCount to 6
                     itemBuilder: (BuildContext context, int index) {
                       return Padding(
                         padding: EdgeInsets.all(8.0), // Add padding as needed
                         child: InkWell(
-                          onTap: (){},
+                          onTap: () {
+                            // Navigate to the corresponding city post screen
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => cityRoutes[index],
+                              ),
+                            );
+                          },
                           child: Container(
                             height: 200,
-                            decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.circular(15),
-                              image: DecorationImage(
-                                image: AssetImage(
-                                  "assets/images/city${index + 1}.jpg",
-                                ),
-                                fit: BoxFit.cover,
-                              ),
-
-                            ),
-                            child: Row(
+                            child: Stack(
                               children: [
                                 Container(
-                                  alignment: Alignment.bottomLeft,
-                                  child: RatingBar.builder(
-                                    // itemBuilder: (BuildContext context, int index) {  }, onRatingUpdate: (double value) {  },
-                                    initialRating: 3,
-                                    minRating: 1,
-                                    itemSize: 27,
-                                    unratedColor: Colors.white,
-                                    direction: Axis.horizontal,
-                                    allowHalfRating: true,
-                                    itemCount: 5,
-                                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                                    itemBuilder: (context, _) => Icon(
-                                      Icons.favorite,
-                                      color: Colors.amber,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(15),
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                        "assets/images/city${index + 1}.jpg",
+                                      ),
+                                      fit: BoxFit.cover,
+                                      opacity: 0.7,
                                     ),
-                                    onRatingUpdate: (rating) {
-                                      print(rating);
-                                    },
-                                  )
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  left: 0,
+                                  right: 0,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          // Access the corresponding text for each index
+                                          cityNames[index],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: RatingBar.builder(
+                                          initialRating: 3,
+                                          minRating: 1,
+                                          itemSize: 27,
+                                          unratedColor: Colors.white,
+                                          direction: Axis.horizontal,
+                                          allowHalfRating: true,
+                                          itemCount: 5,
+                                          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          itemBuilder: (context, _) => Icon(
+                                            Icons.favorite,
+                                            color: Colors.amber,
+                                          ),
+                                          onRatingUpdate: (rating) {
+                                            print(rating);
+                                          },
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
-
-
                           ),
-
                         ),
                       );
                     },
@@ -216,3 +274,4 @@ class Homepage extends StatelessWidget {
     );
   }
 }
+
